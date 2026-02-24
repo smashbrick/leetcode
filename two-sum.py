13a46a625469c6dc5418a4b11691fd224ee21b64
Smashbrick <smashbrick@yahoo.com>
2026-02-24 12:22:22 +0530

    Two sum

---
+++ two-sum.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def twoSum (self, nums:List[int], target:int) -> List[int]:
+        seen = {}
+        for i , num in enumerate(nums):
+            required = target - num
+            if required in seen:
+                return [seen[required], i]
+            seen[num] = i
+               
