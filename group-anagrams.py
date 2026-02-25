2a8f32b758aec801d4a327b0e33b80ea43b232e6
Ankith <smashbrick@yahoo.com>
2026-02-26 03:55:54 +0530

    added alternate solution to group anagrams

--- group-anagrams.py
+++ group-anagrams.py
@@ -12,3 +12,15 @@ class Solution:
             else:
                 groups[tupleIt] = [s]
         return list(groups.values())
+    
+#Alternate solution that is simpler
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        for s in strs:
+            key = tuple(sorted(s))
+            if key not in groups:
+                groups[key] = [s]
+            else:
+                groups[key].append(s)
+        return list(groups.values())
